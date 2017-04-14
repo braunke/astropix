0062a2416b02dc510d3ef533fc79110825834985
d <d>
2017-04-13 20:54:28 -0500

    delete almost working

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
+var session = require('express-session');
+var MongoDBStore = require('connect-mongodb-session')(session);
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var favorites = require('./routes/favorites');
 
 var app = express();
 
@@ -29,8 +31,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configure sessions, and session storage in MongoDB
+var mongo_pw = process.env.MONGO_PW;
+var url = 'mongodb://localhost:27017/hello'
+var store = new MongoDBStore({
+  uri : url,
+      collection : 'sessions'
+}, function(error) {
+  // todo deal with error connection
+     if (error) console.log(error)
+    });
+    app.use(session({
+    secret: "put a random string here",
+    resave: false,
+    saveUninitialized: false,
+    store: store
+     }));
+
 app.use('/', index);
-app.use('/users', users);
+app.use('/favorites', favorites);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/favorites.js
@@ -0,0 +1,43 @@
+//route handlers for adding favorite
+
+var express = require('express');
+var router = express.Router();
+
+/* GET favorites page. */
+router.get('/', function(req, res, next) {
+    res.render('favorites', {favorites : req.session.favorites});
+
+});
+
+/* POST Add new favorite */
+router.post('/add', function(req, res, next){
+    console.log(req.body.date)
+// If no favorites array, create one
+   if (!req.session.favorites) {
+   req.session.favorites = [] ; //Create empty array
+ }
+// Check if this image is already in array
+  for (var x = 0 ; x < req.session.favorites.length ; x++) {
+    if (req.session.favorites[x].date == req.body.date) {
+     console.log('This is already a favorite');
+    return res.redirect('back');   // Back to previous page
+   }
+ }
+  // If not, add to array and redirect to favorites page
+  req.session.favorites.push(req.body);
+  res.redirect('/favorites');
+ });
+router.post('/delete', function(req, res, next){
+    console.log(req.body.date);
+ for (var x =0; x < req.session.favorites.length; x++){
+     if (req.session.favorites[x].date == req.body.date){
+
+
+         req.session.favorites.remove(req.body);
+         res.redirect('back');
+     }
+ }
+ res.redirect('/favorites');
+
+});
+ module.exports = router;
